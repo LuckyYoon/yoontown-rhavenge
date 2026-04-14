15dfb73a4cb67cebf9a275330a2d7f485a00cb8f
MatthewT1107 <mtownsend1107@gmail.com>
2026-04-14 12:02:20 -0400

    4/14 end of class changes

--- testgame.py
+++ testgame.py
@@ -6,7 +6,7 @@ import time
 
 #Initialize pygame
 pygame.init()
-WIN_W, WIN_H    = 960, 720
+WIN_W, WIN_H = 960, 720
 screen = pygame.display.set_mode((WIN_W, WIN_H))
 pygame.display.set_caption("Hell")
 run = True
@@ -37,19 +37,69 @@ class Controller:
         if keys[pygame.K_s]:
             player.y += player.movespeed
 
+#class View:
+
+   # def __init__
+
+class Boss:
+
+    def __init__(self,X,Y):
+        self.X = X
+        self.Y = Y
+        self.HP = 1000
+        self.SIZE = 30
+    
+    #def attack1(self):
+
+class Projectile:
+
+    def __init__(self,p_speed,p_size,p_damage,p_x,p_y):
+        self.p_speed = p_speed  
+        self.p_size = p_size
+        self.p_damage = p_damage
+        self.p_x = p_x
+        self.p_y = p_y
+    def create_projectile(self):
+        self.p_x += self.p_speed
+        self.p_y += self.p_speed
+        pygame.draw.circle(screen, (50, 200, 50), (self.p_x,self.p_y), self.p_size)
+
+class BossProjectile(Projectile):
+    
+    def player_collision(self):
+        dist = math.hypot(instance.x - self.p_x, instance.y - self.p_y)
+        if dist < (0.9*self.p_size + instance.size):
+            pygame.quit()
+            run = False
+
+
+    #If player get hits
+   # def hit_player(self):
+
+        
+
+    
+
+    
 
 instance = Player(50,20)
 controller = Controller()
+#base_projectile = Projectile(2,10,5,50,50)
+boss_proj = BossProjectile(2,10,5,50,50)
+
 while run:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
          run = False
+   
     controller.move(instance)
     screen.fill((0, 0, 0))  # clear screen
     pygame.draw.circle(screen, (50, 200, 50), (instance.x,instance.y), 8)
+    boss_proj.create_projectile()
+    boss_proj.player_collision()
     pygame.display.flip()
-    if pygame.time.get_ticks() >= 2000:
+    if pygame.time.get_ticks() >= 30000:
         pygame.quit()
         run = False
        
